80011868606ef1406d28909751f1ad4e239332de
Maksym Bykov <maksym.bykov@ssa.group>
2025-07-07 21:14:35 +0300

    navigation with arrows

--- public/player.js
+++ public/player.js
@@ -1,7 +1,7 @@
 
 let inLoop = false;
 let subtitles = [];
-let currentSubtitleIndex = -1;
+let loopId = 0;
 const video = document.getElementById('video');
 const subtitleDiv = document.getElementById('subtitle');
 const offset = 0.3; // смещение для перемотки назад
@@ -31,13 +31,16 @@ function parseSRT(data) {
   const subs = [];
   const regex = /(\d+)\s+(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3})\s+([\s\S]*?)(?=\n{2,}|$)/g;
   let match;
+  let counter = 0;
   while ((match = regex.exec(data)) !== null) {
     subs.push({
+      id: counter,
       index: parseInt(match[1]),
       start: timeStrToSec(match[2]),
       end: timeStrToSec(match[3]),
       text: match[4].replace(/\n/g, ' ')
     });
+    counter++;
   }
   return subs;
 }
@@ -52,23 +55,19 @@ video.addEventListener('timeupdate', () => {
   if (!subtitles.length) return;
   const t = video.currentTime;
 
-  let index = Math.max(currentSubtitleIndex - 1, 0);
-  if (index >= 0 && index < subtitles.length - 1) {
-    let currentEnd = index >= 0 ? subtitles[index].end : 0;
-
-    if (inLoop && index >= 0 && t >= currentEnd) {
-      // если включен цикл, перематываем на начало текущих субтитров
-      video.currentTime = subtitles[index].start - offset;
-      return;
+  if(inLoop) {
+    if (t > subtitles[loopId].end ) {
+      video.currentTime = subtitles[loopId].start - offset;
+      subtitleDiv.textContent = subtitles[loopId].text;
     }
   }
+  else {
+    const sub = subtitles.find(s => t >= s.start && t <= s.end);
+    let subContent = sub ? sub.text : '';
 
-  const sub = subtitles.find(s => t >= s.start && t <= s.end);
-  let subContent = sub ? sub.text : '';
-
-  if (sub) currentSubtitleIndex = sub.index
-
-  subtitleDiv.textContent = subContent;
+    if (sub) loopId = sub.id;
+    subtitleDiv.textContent = subContent;
+  }
 });
 
 // управление клавишами
@@ -76,12 +75,12 @@ document.addEventListener('keydown', (e) => {
   if (!subtitles.length) return;
 
   if (e.code === 'ArrowRight') {
-    currentSubtitleIndex = Math.min(currentSubtitleIndex + 1, subtitles.length - 1);
-    video.currentTime = subtitles[currentSubtitleIndex].start;
+    loopId = Math.min(loopId + 1, subtitles.length - 1);
+    video.currentTime = subtitles[loopId].start;
 
   } else if (e.code === 'ArrowLeft') {
-    currentSubtitleIndex = Math.max(currentSubtitleIndex - 2, 0);
-    let startTime = subtitles[currentSubtitleIndex].start;
+    loopId = Math.max(loopId - 1, 0);
+    let startTime = subtitles[loopId].start;
     if (startTime > offset) startTime -= offset;
     video.currentTime = startTime;
 
